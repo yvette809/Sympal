1c95c6001fccfeef354f9045f6955bf55c88d44d
yvette809 <60625477+yvette809@users.noreply.github.com>
2025-05-14 13:09:35 +0200

    added jsonignore

--- backend/src/main/java/com/sympal/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/sympal/backend/config/SecurityConfig.java
@@ -98,7 +98,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // frontend
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:3001"
+
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
